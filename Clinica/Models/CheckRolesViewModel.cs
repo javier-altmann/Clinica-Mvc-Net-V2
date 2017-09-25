af9410b304f9984ef6ca12481b200450c8035ea1
Guido Zaglul <guido.zaglul@outlook.com>
2017-09-25 19:28:05 -0300

    Recibir el rol seleccionado en el action method RedireccionarSegunRol

--- Clinica/Models/CheckRolesViewModel.cs
+++ Clinica/Models/CheckRolesViewModel.cs
@@ -8,8 +8,8 @@ using System.Web.Mvc;
 namespace Clinica.Models
 {
     public class CheckRolesViewModel 
-    {
-        public IEnumerable<SelectListItem> RolesAsignados { get; set; }//esto es para hacer una lista de opciones del combo
+    {        
+        public CheckRolesViewModel() { }
 
         public CheckRolesViewModel(IEnumerable<Rol> Roles)
         {
@@ -18,11 +18,10 @@ namespace Clinica.Models
                 Value = x.Id_Rol.ToString(),
                 Text = x.Descripcion
             });
-
-
-
-
         }
+
+        public IEnumerable<SelectListItem> RolesAsignados { get; set; }//esto es para hacer una lista de opciones del combo
+        public string RolSeleccionado { get; set; }
     }
 
 }
